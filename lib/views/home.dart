b57c33efaed0e8988646d25ec14270387d1bafb4
KizutoFR <yanisbenamor38@gmail.com>
2022-03-03 10:42:41 +0100

    home.dart checkpoint

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -31,32 +31,41 @@ class home extends StatelessWidget {
           padding: const EdgeInsets.all(25.0),
           child: Stack(
             children: [
-              Positioned(child: Image.asset("assets/meteo-nuage-test.jpg"),left: 0, top:0,width: MediaQuery.of(context).size.width,),
               Container(
-                child: SingleChildScrollView(
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: const [
-                      
-                      ListTile(
-                        leading: Icon(Icons.cloud,color: Colors.white),
-                        title: Text("Cloudy"),
-                      ),
-
-                      ListTile(
-                        leading: Icon(Icons.flag,color: Colors.white),
-                        title: Text("20km/h"),
-                      ),
-
-                      ListTile(
-                        leading: Icon(Icons.water ,color: Colors.white),
-                        title: Text("Cloudy"),
-                      ),
-                    ]
+                width: MediaQuery.of(context).size.width,
+                height: 170,
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.only(topLeft: Radius.circular(20), topRight: Radius.circular(20), bottomLeft: Radius.circular(20), bottomRight: Radius.circular(20),),
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image:AssetImage("assets/meteo-nuage-test.jpg"),
+                    ),
                   ),
+              ),
+              SingleChildScrollView(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: const [
+                    
+                    ListTile(
+                      leading: Icon(Icons.cloud,color: Colors.white),
+                      title: Text("Cloudy"),
+                    ),
+
+                    ListTile(
+                      leading: Icon(Icons.flag,color: Colors.white),
+                      title: Text("20km/h"),
+                    ),
+
+                    ListTile(
+                      leading: Icon(Icons.water ,color: Colors.white),
+                      title: Text("Cloudy"),
+                    ),
+                  ],
                 ),
-              )
+              ),
+              
             ]
           )
         )
