0b556217ee70dfbf8cc3711fd346cfb849164b2d
KizutoFR <yanisbenamor38@gmail.com>
2022-03-03 16:34:20 +0100

    Sun Rise

--- lib/widgets/hour_card.dart
+++ lib/widgets/hour_card.dart
@@ -1,43 +1,38 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-Widget hourCard(){
-  return Container(
-    width: 75,
-    child: Card(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(60.0),
+Widget hourCard(BuildContext context,int color,IconData meteoicons){
+  return Padding(
+    padding: const EdgeInsets.all(0.0),
+    child: Container(
+      width: (MediaQuery.of(context).size.width)/2-35,
+      decoration: BoxDecoration(
+        borderRadius: const BorderRadius.all(Radius.circular(20)),
+        color: Color(color),
       ),
-      elevation: 5,
       child: Column(
-        children: const [
-          Padding(
-            padding: const EdgeInsets.only(top:15.0),
-            child: Text(
-              "13"+" h",
-              style: TextStyle(
-                fontSize: 16,
+        mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: EdgeInsets.all(5.0),
+              child:Icon(
+                meteoicons,color: Color.fromARGB(131, 255, 255, 255),size: 55,
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(5.0),
-            child:Icon(
-              Icons.cloud,color: Colors.black,size: 50,
+            Padding(
+              padding: const EdgeInsets.all(5.0),
+              child: Text(
+                "13"+" h",
+                style: TextStyle(
+                  fontSize: 25,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              )
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(5.0),
-            child: Text(
-              "13"+" h",
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-            )
-          ),
-        ],
-      ) ,
-    )
+          ],
+        ) ,
+    ),
   );
 }
