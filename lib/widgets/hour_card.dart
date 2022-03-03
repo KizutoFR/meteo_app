812327a03a5b75df6028270e4f77e60316ddab4e
KizutoFR <yanisbenamor38@gmail.com>
2022-03-03 15:33:59 +0100

    Card prete a etre testé

--- lib/widgets/hour_card.dart
+++ lib/widgets/hour_card.dart
@@ -2,5 +2,42 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 Widget hourCard(){
-  return Text('HOURSCARD');
+  return Container(
+    width: 75,
+    child: Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(60.0),
+      ),
+      elevation: 5,
+      child: Column(
+        children: const [
+          Padding(
+            padding: const EdgeInsets.only(top:15.0),
+            child: Text(
+              "13"+" h",
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(5.0),
+            child:Icon(
+              Icons.cloud,color: Colors.black,size: 50,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: Text(
+              "13"+" h",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            )
+          ),
+        ],
+      ) ,
+    )
+  );
 }
