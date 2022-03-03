50f5c8cabe3a964b57a11fd0b56b6d6516d5e617
Lucas Boinet <42383760+lucasboinet@users.noreply.github.com>
2022-03-03 16:40:12 +0100

    fixed title

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:meteo_app/views/home.dart';
+import 'package:meteo_app/views/homepage.dart';
 
 void main() {
   runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
     title: "Météo APP",
-    home: home(),
+    home: homepage(),
   ));
 }
--- lib/views/homepage.dart
+++ lib/views/homepage.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import '../widgets/hour_card.dart';
+import '../widgets/day_card.dart';
 
-class homePage extends StatelessWidget {
-  const homePage({Key? key}) : super(key: key);
+class homepage extends StatelessWidget {
+  const homepage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -12,111 +13,251 @@ class homePage extends StatelessWidget {
         gradient: LinearGradient(
           begin: Alignment.topRight,
           end: Alignment.bottomLeft,
-          colors: [Color(0xffEFF0FF), Color(0xffC3D1FF)]
-          )
-        ),
-      child: Scaffold(
-        backgroundColor: Colors.transparent,
-        appBar: AppBar(
-          title: const Text(
-                            "Bonjour Lucas", 
-                            style: TextStyle(
-                              color: Color(0xff353D65)
-                            )
-                          ),
-          backgroundColor: Colors.transparent,
-          elevation: 0.0,
-        ),
-        body: Column( 
-          children:[
-            Padding(
-              padding: const EdgeInsets.all(25.0),
-              child: Stack(
-                children: [
-                  Positioned(child: Image.asset("assets/meteo-nuage-test.jpg"),left: 0, top:0,width: MediaQuery.of(context).size.width,),
-                  SizedBox(
-                    height: 200,
-                    child:Row(
-                    children: [
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: const [
-                          
-                           ListTile(
-                            leading: Icon(Icons.cloud,color: Colors.white),
-                            title: Text("Cloudy"
-                                ),
-                          ),
-
-                           ListTile(
-                            leading: Icon(Icons.flag,color: Colors.white),
-                            title: Text("20km/h" 
-                              ),
-                          ),
-
-                           ListTile(
-                            leading: Icon(Icons.water ,color: Colors.white),
-                            title: Text("50%"
-                                ),
-                          ),
-                          
-
-                          
-                          
-                      ],),
-                      Column(
-                        children: [
-                            const Text("15°",style: TextStyle(
-                                fontSize: 100.0,
-                                color: Colors.white,
-                                letterSpacing: 0.7,
-                              ), ),
-                            Row(
-                              children: [
-                                Row(
-                                  children: const [
-                                    Icon(Icons.arrow_downward ,color: Colors.white),
-                                    Text("50%",style: TextStyle(
-                                      fontSize: 20.0,
-                                      color: Colors.white,
-                                      letterSpacing: 0.7,
-                                    ), )
-                                  ],
-                                ),
-                                Row(
-                                  children: const [
-                                    Icon(Icons.arrow_upward ,color: Colors.white),
-                                    Text("50%",style: TextStyle(
-                                      fontSize: 20.0,
-                                      color: Colors.white,
-                                      letterSpacing: 0.7,
-                                    ), )
-                                  ],
-                                ),
-                            ],
-                          )
-                        ],
-                      ),
-                       
-                    ],
-                  )
-                  ),
-                ]
+          colors: [Color(0xffEFF0FF), Color(0xffC3D1FF)])),
+          child: Scaffold(
+            backgroundColor: Colors.transparent,
+            appBar: AppBar(
+              title: const Text("Lyon",
+                style: TextStyle(
+                  color: Color(0xff353D65),
+                )
               ),
+              backgroundColor: Colors.transparent,
+              elevation: 0.0,
             ),
-            Padding(
-              padding: const EdgeInsets.all(10.0),
+            body: Container (
               child: Row(
-                children: const[
-                  Text("Leve de soleil"),
-                  Text("Couché du soleil")
+                children: [
+                  Expanded(
+                    child: SingleChildScrollView(
+                      child: Column(children: [
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(25, 10, 25, 10),
+                          child: Container(
+                            width: MediaQuery.of(context).size.width,
+                            height: 170,
+                            decoration: const BoxDecoration(
+                              borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(20),
+                                topRight: Radius.circular(20),
+                                bottomLeft: Radius.circular(20),
+                                bottomRight: Radius.circular(20),
+                              ),
+                              gradient: LinearGradient(
+                              begin: Alignment.topRight,
+                              end: Alignment.bottomLeft,
+                              colors: [Color(0xff5672FD), Color(0xff758AF6)])
+                            ),
+                            child: Row(    
+                              mainAxisSize: MainAxisSize.min,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [   
+                                Expanded(
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      RichText(
+                                        text: const TextSpan(
+                                        children: [
+                                          WidgetSpan(
+                                            child: Padding(
+                                              padding: EdgeInsets.only(right:8.0),
+                                              child: Icon(Icons.cloud,
+                                                  color: Color.fromARGB(166, 211, 209, 209), size: 24),
+                                            ),
+                                          ),
+                                          TextSpan(
+                                            text: "Cloudy ",
+                                            style: TextStyle(
+                                              color: Colors.white,
+                                              height: 2,
+                                              fontSize: 20,
+                                            )
+                                          ),
+                                        ],),
+                                      ),
+                                      RichText(
+                                        text: const TextSpan(
+                                          children: [
+                                          WidgetSpan(
+                                            child: Padding(
+                                              padding: EdgeInsets.only(right:8.0),
+                                              child: Icon(
+                                                Icons.flag,
+                                                color: Color.fromARGB(166, 211, 209, 209), 
+                                                size: 24
+                                              ),
+                                            ),
+                                          ),
+                                          TextSpan(
+                                            text: "20 km/h",
+                                            style: TextStyle(
+                                              color: Colors.white,
+                                              height: 2,
+                                              fontSize: 20,
+                                            )
+                                          ),],
+                                        ),
+                                      ),
+                                      RichText(
+                                        text: const TextSpan(
+                                          children: [
+                                            WidgetSpan(
+                                              child: Padding(
+                                                padding: EdgeInsets.only(right:8.0),
+                                                child: Icon(
+                                                  Icons.water,
+                                                  color: Color.fromARGB(166, 211, 209, 209),
+                                                  size: 24
+                                                ),
+                                              ),
+                                            ),
+                                            TextSpan(
+                                              text: "50% ",
+                                              style: TextStyle(
+                                                color: Colors.white,
+                                                height: 2,
+                                                fontSize: 20,
+                                              )
+                                            ),
+                                          ],
+                                        ),
+                                      ),                                       
+                                    ],
+                                  ),
+                                ),
+                                Expanded(
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children:  [                                  
+                                      const Text("15"+"°",
+                                        style:TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 75.0,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                      Row(
+                                        mainAxisAlignment: MainAxisAlignment.center,
+                                        children: [
+                                          RichText(
+                                            text: const TextSpan(
+                                              children: [
+                                                WidgetSpan(
+                                                  child: Icon(
+                                                    Icons.arrow_downward,
+                                                    color: Color.fromARGB(131, 230, 229, 229),
+                                                    size: 24
+                                                  ),
+                                                ),
+                                                TextSpan(
+                                                  text: "12°",
+                                                  style: TextStyle(
+                                                    color: Colors.white,
+                                                    fontSize: 20,
+                                                  )
+                                                ),
+                                              ],
+                                            ),
+                                          ),
+                                          RichText(
+                                            text: const TextSpan(
+                                              children: [
+                                                WidgetSpan(
+                                                  child: Icon(
+                                                    Icons.arrow_upward,
+                                                    color: Color.fromARGB(166, 211, 209, 209), 
+                                                    size: 24
+                                                  ),
+                                                ),
+                                                TextSpan(
+                                                  text: "18°",
+                                                  style: TextStyle(
+                                                    color: Colors.white,
+                                                    fontSize: 20,
+                                                  )
+                                                ),
+                                              ],
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 25, top: 25, bottom: 10),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: const [
+                              Text(
+                                "Sun schedule",
+                                style: TextStyle(
+                                  fontSize: 24,
+                                  fontWeight: FontWeight.bold,
+                                  color: Color(0xff353D65)
+                                ),
+                              )
+                            ]
+                          ),
+                        ),
+                        SizedBox(
+                          height: 160,
+                          child: Padding(
+                            padding: const EdgeInsets.only(left: 25, top: 10, right: 25),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                hourCard(context,0xff7e92f7,Icons.wb_sunny_rounded),
+                                hourCard(context,0xff5b76fc,Icons.mode_night_rounded),
+                              ],
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 25, top: 25, bottom: 10),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: const [
+                              Text(
+                                "Next 5 days",
+                                style: TextStyle(
+                                  fontSize: 24,
+                                  fontWeight: FontWeight.bold,
+                                  color: Color(0xff353D65)
+                                ),
+                              )
+                            ]
+                          ),
+                        ),
+                        IntrinsicHeight(
+                          child: Padding(
+                            padding: const EdgeInsets.fromLTRB(15, 10, 15, 0),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.stretch,
+                              children: [
+                                dayCard(),
+                                dayCard(),
+                                dayCard(),
+                                dayCard(),
+                                dayCard(),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ]
+                    ),
+                    ),
+                  ),
                 ],
               )
-            )
-            
-          ]
-        ),
-      )
-    );
+          )  
+        )
+      );
   }
 }
